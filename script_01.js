e8011db8acab65d3afea5fac37c7b6513b640996
Matthias_vdM <matthias.7@web.de>
2022-07-12 13:07:16 +0200

    Funktionen update

--- script_01.js
+++ script_01.js
@@ -28,10 +28,58 @@ function ausgabeNamen() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamenParam("Anna"); // Argument 
-ausgabeNamenParam("Craig");
-ausgabeNamenParam("Fatih");
+// ausgabeNamenParam("Anna"); // Argument 
+// ausgabeNamenParam("Craig");
+// ausgabeNamenParam("Fatih");
 
 function ausgabeNamenParam(firstName) { // Parameter
     console.log("Hallo " + firstName + "!");
 }
+
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter
+
+// ausgabeNamenParams("Maxine","Mütze");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+
+function ausgabeNamenParams(firstName, familyName) { // Parameter
+    console.log("Hallo " + firstName + " " + familyName + "!");
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenParams2("Mütze","Max");
+
+function ausgabeNamenParams2(firstName, familyName) { // Parameter
+
+     // 1. Funktionalität: string composing
+    const gap = " ";
+    const outputStr = "Hallo"+ gap + firstName + gap + familyName + "!";
+
+    // 2. Funktionalität: data output
+    console.log(outputStr);
+
+}
+
+/***** Funktionen 03b *****/
+// Trennen der Funktionalitäten | return
+
+
+
+ // 1. Funktionalität: string composing
+output(getString("Maxine","Mütze"));
+function getString(firstName, familyName) {
+    const gap = " ";
+    const outputStr = "Hallo"+ gap + firstName + gap + familyName + "!";
+    return outputStr; // ---> Daten zum Call
+    console.log("hi"); // return BEENDET die Funktion!
+}
+
+ // 2. Funktionalität: data output
+//  output("Hi");
+ function output(outputData) {
+    console.log(outputData);
+ }
